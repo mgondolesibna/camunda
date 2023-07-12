2c3ea8f0251b22682a22c710b4a89dc44f3b2005
Mariano <mgondolesi@gmail.com>
2023-07-12 17:16:14 -0300

    form dinamico que se activa cuando la instancia llega a la TAREA 2

--- react/formulario-prestamo/src/MyForm.js
+++ react/formulario-prestamo/src/MyForm.js
@@ -1,91 +1,59 @@
-// import React, { useState } from 'react';
-// import axios from 'axios';
-// import { Form, Input, InputNumber, Button } from 'antd';
-
-// const MyForm = () => {
-//   const [nombre, setNombre] = useState('');
-//   const [apellido, setApellido] = useState('');
-//   const [cantidad, setCantidad] = useState(0);
-//   const [plazo, setPlazo] = useState(0);
-
-//   const handleSubmit = async (values) => {
-//     console.log('Submit button clicked', values);
-
-//     try {
-//       const username = 'demo';
-//       const password = 'demo';
-//       const authString = `${username}:${password}`;
-//       const encodedAuthString = btoa(authString);
-
-//       const headers = {
-//         'Authorization': `Basic ${encodedAuthString}`
-//       };
-
-//       const response = await axios.post(
-//         '/api/engine/default/process-definition/key/poc-camunda-2-process/start',
-//         {
-//           variables: {
-//             nombre: { value: values.nombre, type: 'String' },
-//             apellido: { value: values.apellido, type: 'String' },
-//             cantidad: { value: values.cantidad, type: 'Integer' },
-//             plazo: { value: values.plazo, type: 'Integer' },
-//           },
-//         },
-//         { headers }
-//       );
-
-//       console.log(response.data);
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   };
-
-//   return (
-//     <Form onFinish={handleSubmit} style={{ width: 400, margin: '0 auto' }}>
-//       <Form.Item label="Nombre" name="nombre" rules={[{ required: true }]}>
-//         <Input value={nombre} onChange={(e) => setNombre(e.target.value)} />
-//       </Form.Item>
-//       <Form.Item label="Apellido" name="apellido" rules={[{ required: true }]}>
-//         <Input value={apellido} onChange={(e) => setApellido(e.target.value)} />
-//       </Form.Item>
-//       <Form.Item
-//         label="Cantidad de dinero a solicitar"
-//         name="cantidad"
-//         rules={[{ required: true, type: 'number', min: 0 }]}
-//       >
-//         <InputNumber value={cantidad} onChange={(value) => setCantidad(value)} min={0} />
-//       </Form.Item>
-//       <Form.Item
-//         label="Plazo para devolverlo"
-//         name="plazo"
-//         rules={[{ required: true, type: 'number', min: 0 }]}
-//       >
-//         <InputNumber value={plazo} onChange={(value) => setPlazo(value)} min={0} />
-//       </Form.Item>
-//       <Form.Item>
-//         <Button type="primary" htmlType="submit">
-//           Iniciar proceso
-//         </Button>
-//       </Form.Item>
-//     </Form>
-//   );
-// };
-
-// export default MyForm;
-
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Form, Input, Select, Checkbox, Button, message } from 'antd';
+import { Form, Input, Select, Checkbox, Button, message, Spin } from 'antd';
 
 const { Option } = Select;
 
 const MyForm = () => {
   const [tipoCuenta, setTipoCuenta] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [showSecondForm, setShowSecondForm] = useState(false);
+  const [responseId, setResponseId] = useState('');
+
+  useEffect(() => {
+    if (showSecondForm) {
+      setLoading(false);
+    }
+  }, [showSecondForm]);
+
+  const handleSubmitCuit = async (value) => {
+    console.log('Cuit enviado:', value);
+
+    try {
+      const username = 'demo';
+      const password = 'demo';
+      const authString = `${username}:${password}`;
+      const encodedAuthString = btoa(authString);
+
+      const headers = {
+        'Authorization': `Basic ${encodedAuthString}`
+      };
+
+      const response = await axios.put(
+        '/api/engine/default/process-instance/' + responseId + '/variables/cuit',
+        {
+          value: value.cuit,
+          type: 'String',
+        },
+        { headers }
+      );
+
+      console.log(response.data);
+
+      // Mostrar mensaje de éxito
+      message.success('Su CUIT ha sido enviado exitosamente');
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const handleSubmit = async (values) => {
     console.log('Formulario enviado:', values);
 
     try {
+      setLoading(true); // Mostrar la ruedita de carga
+
       const username = 'demo';
       const password = 'demo';
       const authString = `${username}:${password}`;
@@ -109,14 +77,32 @@ const MyForm = () => {
       );
 
       console.log(response.data);
+      const { id } = response.data;
+      setResponseId(id); // Guardar el responseId en el estado
 
       // Mostrar mensaje de éxito
       message.success('Su solicitud ha sido enviada exitosamente');
 
-      // Retraso de 3 segundos antes de recargar la página
-      setTimeout(() => {
-        window.location.reload();
-      }, 3000);
+      // Realizar solicitud GET cada 5 segundos hasta que el campo childActivityInstances.activityId sea "Activity_16s9fbv"
+      const intervalId = setInterval(async () => {
+        try {
+          const activityInstancesResponse = await axios.get('http://localhost:8080/engine-rest/engine/default/process-instance/' + id + '/activity-instances');
+          console.log("holaaaaaaaaaaaaaaaaaaa");
+          const activityInstances = activityInstancesResponse.data.childActivityInstances;
+          console.log(activityInstances);
+          activityInstances.forEach((element) => {
+            if (element.activityId === 'Activity_16s9fbv') {
+              setLoading(false); // Ocultar la ruedita de carga
+              setShowSecondForm(true); // Mostrar el segundo formulario
+              message.info('Por favor ingrese su CUIT');
+              clearInterval(intervalId);
+            }
+          })
+        } catch (error) {
+          console.error(error);
+        }
+      }, 5000); // Retraso de 5 segundos
+
     } catch (error) {
       console.error(error);
     }
@@ -130,36 +116,61 @@ const MyForm = () => {
     // Aquí puedes realizar alguna acción si es necesario
   };
 
+  if (showSecondForm) {
+    return (
+      <Spin spinning={loading}>
+        {/* Ruedita de carga */}
+        <Form
+          name="cuitForm"
+          style={{ width: 400, margin: '0 auto' }}
+          onFinish={handleSubmitCuit}
+        >
+          <Form.Item label="CUIT" name="cuit" rules={[{ required: true, message: 'Por favor ingresa tu CUIT' }]}>
+            <Input />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Enviar CUIT
+            </Button>
+          </Form.Item>
+        </Form>
+      </Spin>
+    );
+  }
+
   return (
-    <Form
-      name="loanForm"
-      initialValues={{ tipoCuenta: '', emitirChequera: false }}
-      onFinish={handleSubmit}
-      style={{ width: 400, margin: '0 auto' }}
-    >
-      <Form.Item label="Nombre" name="nombre" rules={[{ required: true, message: 'Por favor ingresa tu nombre' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Apellido" name="apellido" rules={[{ required: true, message: 'Por favor ingresa tu apellido' }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item label="Tipo de cuenta" name="tipoCuenta" rules={[{ required: true, message: 'Por favor selecciona el tipo de cuenta' }]}>
-        <Select onChange={handleTipoCuentaChange}>
-          <Option value="10">Cuenta Corriente</Option>
-          <Option value="27">Cuenta Corriente Especial</Option>
-        </Select>
-      </Form.Item>
-      {tipoCuenta === '10' && (
-        <Form.Item name="emitirChequera" valuePropName="checked">
-          <Checkbox onChange={handleEmitirChequeraChange}>Emitir chequera</Checkbox>
+    <Spin spinning={loading}>
+      {/* Ruedita de carga */}
+      <Form
+        name="loanForm"
+        initialValues={{ tipoCuenta: '', emitirChequera: false }}
+        onFinish={handleSubmit}
+        style={{ width: 400, margin: '0 auto' }}
+      >
+        <Form.Item label="Nombre" name="nombre" rules={[{ required: true, message: 'Por favor ingresa tu nombre' }]}>
+          <Input />
         </Form.Item>
-      )}
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Iniciar proceso
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item label="Apellido" name="apellido" rules={[{ required: true, message: 'Por favor ingresa tu apellido' }]}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="Tipo de cuenta" name="tipoCuenta" rules={[{ required: true, message: 'Por favor selecciona el tipo de cuenta' }]}>
+          <Select onChange={handleTipoCuentaChange}>
+            <Option value="10">Cuenta Corriente</Option>
+            <Option value="27">Cuenta Corriente Especial</Option>
+          </Select>
+        </Form.Item>
+        {tipoCuenta === '10' && (
+          <Form.Item name="emitirChequera" valuePropName="checked">
+            <Checkbox onChange={handleEmitirChequeraChange}>Emitir chequera</Checkbox>
+          </Form.Item>
+        )}
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Iniciar proceso
+          </Button>
+        </Form.Item>
+      </Form>
+    </Spin>
   );
 };
 
